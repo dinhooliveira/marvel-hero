cdb05c757847520eb9fc216d4752ef70ad1d6f29
oliveira <oliveira@dbseller.com.br>
2022-01-20 00:35:13 -0300

    incluindo teste unitario

--- app/home/home.componet.spec.js
+++ app/home/home.componet.spec.js
@@ -1,4 +1,20 @@
-describe('home', function() {
-    // Load the module that contains the `phoneList` component before each test
-    beforeEach(module('home'));
+'use strict';
+
+describe('home', function () {
+  // Load the module that contains the `home` component before each test
+  beforeEach(module('home'));
+
+
+  // Test the controller
+  describe('HomeController', function () {
+    var  ctrl;
+    beforeEach(inject(function($componentController) {
+      ctrl = $componentController('home');
+    }));
+
+    it('should  model herois equal []', function () {
+      expect(ctrl.herois.length).toBe(0);
+    });
+
   });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,17 @@ module.exports = function(config) {
       basePath: './app',
   
       files: [
+        'lib/blueimp-md5/js/md5.js',
         'lib/angular/angular.js',
+        'lib/angular-resource/angular-resource.js',
         'lib/angular-route/angular-route.js',
         '../node_modules/angular-mocks/angular-mocks.js',
         'core/**/*.js',
-        'view*/**/*.js'
+        'view*/**/*.js',
+        '**/*.module.js',
+        '*!(.module|.spec).js',
+        '!(lib)/**/*!(.module|.spec).js',
+        '**/*.spec.js'
       ],
   
       autoWatch: true,
